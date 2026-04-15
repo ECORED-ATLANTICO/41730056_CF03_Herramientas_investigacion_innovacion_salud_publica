ef25cef28830921d352373f6f39e99d48653f463
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-04-15 07:03:11 -0500

    Ajustes finales

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -39,8 +39,8 @@
     figure.mb-5
       .video.mb-2
         iframe(width="560" height="315" src="https://www.youtube.com/embed/FOlBvWPGPKM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption.text-bold Video: Innovación en salud pública: tecnología y propuesta de valor
-
+      figcaption Video: innovación en salud pública: tecnología y propuesta de valor
+ 
 
 </template>
 
