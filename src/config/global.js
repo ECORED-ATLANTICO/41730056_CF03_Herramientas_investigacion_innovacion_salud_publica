0de0d5150e1bcc94114a1506708cabd7e8603d9a
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-04-23 08:09:35 -0500

    Ajustes
    Se actualizan creditos

--- src/config/global.js
+++ src/config/global.js
@@ -352,7 +352,7 @@ export default {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
             'Responsable del Ecosistema de Recursos Educativos Digitales (RED)',
-          centro: 'Dirección General',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
         {
           nombre: 'Miguel de Jesús Paredes Maestre',
